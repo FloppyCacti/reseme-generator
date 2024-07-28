8a8ebcd6bb15dcce4e80fba82f1fea667fdb57b6
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-07-28 14:58:40 -0500

    import and add function to show resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import { Builder } from "./components/Builder.jsx";
+import { ShowResume } from "./components/Resume.jsx";
 
 function App() {
   return (
     <>
       <Builder />
+      <ShowResume />
     </>
   );
 }
