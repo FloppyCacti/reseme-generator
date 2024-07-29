baae33f05bda609129f6625e6b0e5bdc55d28997
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-07-29 14:00:50 -0500

    create buttons to store and remove school info

--- src/components/SchoolInfo.jsx
+++ src/components/SchoolInfo.jsx
@@ -1,4 +1,7 @@
+import React, { useRef, useState } from "react";
 import "../styles/container.css";
+import "../styles/SchoolInfo.css";
+import "./Resume";
 
 function SchoolInfo({
   school,
@@ -10,12 +13,49 @@ function SchoolInfo({
   schoolEndDate,
   setSchoolEndDate,
 }) {
+  const [educationContainer, setEducationContainer] = useState([]);
+
+  const schoolRef = useRef(null);
+  const degreeRef = useRef(null);
+  const startDateRef = useRef(null);
+  const endDateRef = useRef(null);
+
+  const insertIntoEducationContainer = (school, degree, startDate, endDate) => {
+    const obj = {
+      school: school,
+      degree: degree,
+      startDate: startDate,
+      endDate: endDate,
+    };
+    setEducationContainer((prev) => [...prev, obj]);
+  };
+
+  const handleAdd = () => {
+    insertIntoEducationContainer(
+      schoolRef.current.value,
+      degreeRef.current.value,
+      startDateRef.current.value,
+      endDateRef.current.value
+    );
+
+    // Clear the input fields
+    setSchool("");
+    setDegree("");
+    setSchoolStartDate("");
+    setSchoolEndDate("");
+  };
+
+  const handleRemove = (index) => {
+    setEducationContainer((prev) => prev.filter((item, i) => i !== index));
+  };
+
   return (
     <>
       <div className="container">
         <div>
           <h2>School:</h2>
           <input
+            ref={schoolRef}
             id="schoolInput"
             type="text"
             value={school}
@@ -25,6 +65,7 @@ function SchoolInfo({
         <div>
           <h2>Degree: </h2>
           <input
+            ref={degreeRef}
             id="degreeInput"
             type="text"
             value={degree}
@@ -34,6 +75,7 @@ function SchoolInfo({
         <div>
           <h2>Start Date:</h2>
           <input
+            ref={startDateRef}
             id="schoolStartInput"
             type="date"
             value={schoolStartDate}
@@ -43,12 +85,29 @@ function SchoolInfo({
         <div>
           <h2>Finish Date:</h2>
           <input
+            ref={endDateRef}
             id="schoolEndInput"
             type="date"
             value={schoolEndDate}
             onChange={(e) => setSchoolEndDate(e.target.value)}
           ></input>
         </div>
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <div id="educationContainer">
+        {educationContainer.map((element, index) => (
+          <div key={index}>
+            <div>
+              <h4>
+                {element.school} - {element.degree}
+              </h4>
+              <h4>
+                {element.startDate} - {element.endDate}
+              </h4>
+            </div>
+            <button onClick={() => handleRemove(index)}>Remove</button>
+          </div>
+        ))}
       </div>
     </>
   );
