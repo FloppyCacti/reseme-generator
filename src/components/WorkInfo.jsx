3b530cfce603e120c63c5682f328bfb487a69e7a
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-07-30 17:45:36 -0500

    add button to store work info

--- src/components/WorkInfo.jsx
+++ src/components/WorkInfo.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "../styles/container.css";
 import "../styles/workInfo.css";
 
@@ -10,13 +11,49 @@ function WorkInfo({
   setJobStartDate,
   jobEndDate,
   setJobEndDate,
+  container,
+  setContainer,
 }) {
+  const positionRef = useRef(null);
+  const companyRef = useRef(null);
+  const startDateRef = useRef(null);
+  const endDateRef = useRef(null);
+
+  const insertIntoWorkContainer = (position, company, startDate, endDate) => {
+    const obj = {
+      position: position,
+      company: company,
+      startDate: startDate,
+      endDate: endDate,
+    };
+    setContainer((prev) => [...prev, obj]);
+  };
+
+  const handleAdd = () => {
+    insertIntoWorkContainer(
+      positionRef.current.value,
+      companyRef.current.value,
+      startDateRef.current.value,
+      endDateRef.current.value
+    );
+
+    // Clear the input fields
+    setPosition("");
+    setCompany("");
+    setJobStartDate("");
+    setJobEndDate("");
+  };
+
+  const handleRemove = (index) => {
+    setContainer((prev) => prev.filter((item, i) => i !== index));
+  };
   return (
     <>
       <div className="container">
         <div>
           <h2>Position/Title:</h2>
           <input
+            ref={positionRef}
             id="positionInput"
             type="text"
             value={position}
@@ -26,6 +63,7 @@ function WorkInfo({
         <div>
           <h2>Company: </h2>
           <input
+            ref={companyRef}
             id="comanyInput"
             type="text"
             value={company}
@@ -35,6 +73,7 @@ function WorkInfo({
         <div>
           <h2>Started at: </h2>
           <input
+            ref={startDateRef}
             id="jobStartInput"
             type="date"
             value={jobStartDate}
@@ -44,13 +83,31 @@ function WorkInfo({
         <div>
           <h2>Ended at: </h2>
           <input
+            ref={endDateRef}
             id="jobEndInput"
             type="date"
             value={jobEndDate}
             onChange={(e) => setJobEndDate(e.target.value)}
           ></input>
         </div>
-        <button>Add</button>
+        <button onClick={handleAdd}>Add</button>
+        {container.length !== 0 && (
+          <div id="workContainer">
+            {container.map((element, index) => (
+              <div key={index}>
+                <div>
+                  <h4>
+                    {element.position} - {element.company}
+                  </h4>
+                  <h4>
+                    {element.startDate} - {element.endDate}
+                  </h4>
+                </div>
+                <button onClick={() => handleRemove(index)}>Remove</button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
